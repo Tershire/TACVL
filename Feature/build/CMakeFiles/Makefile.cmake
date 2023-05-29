5a83ccdd1c157efcb09d4574bdaeb843a1eb23ea
tershire <tershire@gmail.com>
2023-05-29 06:49:22 +0200

    changed name from canny_test_image to canny_edge_image

--- Feature/build/CMakeFiles/Makefile.cmake
+++ Feature/build/CMakeFiles/Makefile.cmake
@@ -56,5 +56,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/canny_test_image.dir/DependInfo.cmake"
+  "CMakeFiles/canny_edge_image.dir/DependInfo.cmake"
   )
