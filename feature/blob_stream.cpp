887b22b6745bbc2f8fe2da528c6fb7dc9b1b0270
tershire <tershire@gmail.com>
2023-05-29 19:27:00 +0200

    completed blob_stream.cpp

--- feature/blob_stream.cpp
+++ feature/blob_stream.cpp
@@ -3,6 +3,7 @@
 // Tershire
 
 // referred:
+// https://eehoeskrap.tistory.com/123
 
 // stream video
 
@@ -11,6 +12,7 @@
 
 // HEADER /////////////////////////////////////////////////////////////////////
 #include "opencv2/opencv.hpp"
+// #include <opencv2/features2d.hpp>
 
 using namespace cv;
 
@@ -19,11 +21,27 @@ using namespace cv;
 int main(int argc, char **argv)
 {
     // Variable ===============================================================
-    Mat frame, frame_edge;
+    Mat frame, frame_blob;
+
+    // detector object
+    Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create();
+
+    //SimpleBlobDetector::Params params;
+	//params.filterByArea = true;
+	//params.minArea = 4;
+	//params.maxArea = 1000;
+	//params.filterByColor = true;
+	//params.blobColor = 0; // dark [0, 255] bright
+	//params.maxThreshold = 170;
+	//params.filterByCircularity = 10;
+	//Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
+
+    // key points
+    std::vector<KeyPoint> keypoints;
 
 
     // Create VideoCapture Object =============================================
-    VideoCapture cap(0);
+    VideoCapture cap(2);
 
     // check capture
     if (!cap.isOpened()) {
@@ -45,15 +63,22 @@ int main(int argc, char **argv)
             break;
         }
 
-        // Canny Edge Detection -----------------------------------------------
-        // apply blur filter (to reduce noise)
-        GaussianBlur(frame, frame, Size(5, 5), 0); // effective for Size > 3
+        // convert color (just for eye relaxation)
+        cvtColor(frame, frame, COLOR_BGR2GRAY);
 
-        // apply Canny edge detector
-        Canny(frame, frame_edge, 50, 100);
+        // Blob Detection -----------------------------------------------------
+        // apply blur filter (to reduce noise)
+        // GaussianBlur(frame, frame, Size(5, 5), 0); // effective for Size > 3
+
+        // apply blob detector
+        detector->detect(frame, keypoints);
+
+        // draw
+        drawKeypoints(frame, keypoints, frame_blob, Scalar(0, 0, 255),
+                      DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
         // show frame ---------------------------------------------------------
-        imshow("Canny Edge Stream", frame_edge);
+        imshow("Canny Edge Stream", frame_blob);
         int key = waitKey(10);
         if (key == 27)
         {
